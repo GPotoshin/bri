cc8342787443b71a91dec32f59f4c366bf930830
George Potoshin <george.potoshin@gmail.com>
2025-03-15 17:56:35 +0100

    attention block done

--- src/transformer.zig
+++ src/transformer.zig
@@ -85,7 +85,7 @@ pub fn matprod(comptime T: type, mat1: Matrix(T), mat2: Matrix(T), out: Matrix(T
 
 /// adds to each row of mat the vect
 /// mat: NxM, vec: M
-pub fn addMatVect(comptime T: type, mat: Matrix(T), vec: []T) !void {
+pub fn addMatRow(comptime T: type, mat: Matrix(T), vec: []T) !void {
     if (mat.width != vec.len) {
         std.debug.print("Incoherent mat and vect sizes!\n", .{});
         return error.IncompatibleObjects;
@@ -99,6 +99,22 @@ pub fn addMatVect(comptime T: type, mat: Matrix(T), vec: []T) !void {
     }
 }
 
+/// adds to each row of mat the vect
+/// mat: NxM, vec: M
+pub fn addMatCol(comptime T: type, mat: Matrix(T), vec: []T) !void {
+    if (mat.height != vec.len) {
+        std.debug.print("Incoherent mat and vect sizes!\n", .{});
+        return error.IncompatibleObjects;
+    }
+
+    // dumb implementation
+    for (0..mat.height) |i| {
+        for (0..mat.width) |j| {
+            mat.ptr[i*mat.width+j] += vec[i];
+        }
+    }
+}
+
 /// memory for the output should be preallocated and the sizes should be coherent
 /// mat: NxM, input: LxM, vect: N, output: LxN
 pub fn affine(comptime T: type, payload: struct {mat: Matrix(T), input: Matrix(T),
@@ -108,21 +124,34 @@ pub fn affine(comptime T: type, payload: struct {mat: Matrix(T), input: Matrix(T
     // const out_dim: u32 = payload.mat.height;
 
     try matprod(T, payload.mat, payload.input, payload.output);
-    try addMatVect(T, payload.output, payload.vect);
+    try addMatRow(T, payload.output, payload.vect);
+}
+
+
+/// memory for the output should be preallocated and the sizes should be coherent
+/// mat: NxM, input: LxM, vect: N, output: LxN
+pub fn affine2(comptime T: type, payload: struct {mat: Matrix(T), input: Matrix(T),
+    vect: []T, output: Matrix(T)}) !void {
+    // the sympliest algo
+    // const in_dim: u32 = payload.mat.width;
+    // const out_dim: u32 = payload.mat.height;
+
+    try matprod(T, payload.mat, payload.input, payload.output);
+    try addMatCol(T, payload.output, payload.vect);
 }
 
 /// funciton overwrites content of mat and does soft max on every row of mat
-pub fn softmax(comptime T: type, mat: Matrix(T)) {
-    for (i..mat.height) |i| {
-        var vect = mat.row(i);
+pub fn softmax(comptime T: type, mat: Matrix(T)) void {
+    for (0..mat.height) |i| {
+        const vect = mat.row(i);
         var sum: T = 0;
 
         for (vect) |*v| {
-            v = std.math.exp(v);
-            sum += v;
+            v.* = @exp(v.*);
+            sum += v.*;
         }
         for (vect) |*v| {
-            v = v/sum;
+            v.* = v.*/sum;
         }
     }
 
@@ -130,8 +159,8 @@ pub fn softmax(comptime T: type, mat: Matrix(T)) {
 
 pub fn Attention(comptime T: type) type {
     return struct {
-        prim_dim: u32,
-        cont_dim: u32,
+        seq_dim: u32,
+        ctx_dim: u32,
         attn_dim: u32,
         out_dim: u32,
 
@@ -149,15 +178,20 @@ pub fn Attention(comptime T: type) type {
         value: Matrix(T),
         score: Matrix(T),
 
-        out_value: Matrix(T),
+        out: Matrix(T),
 
         const Self = @This();
         pub fn init(allocator: std.mem.Allocator, hyper_param: struct {
-            prim_dim: u32, ctx_dim: u32, attn_dim: u32, out_dim: u32,
+            seq_dim: u32, ctx_dim: u32, attn_dim: u32, out_dim: u32,
             seq_len: u32, ctx_len: u32}) !Self {
 
-            const retval: Self = undefined;
-            retval.query_matrix = try Matrix(T).init(allocator, hyper_param.attn_dim, hyper_param.prim_dim);
+            var retval: Self = undefined;
+            retval.seq_dim = hyper_param.seq_dim;
+            retval.ctx_dim = hyper_param.ctx_dim;
+            retval.attn_dim = hyper_param.attn_dim;
+            retval.out_dim = hyper_param.out_dim;
+
+            retval.query_matrix = try Matrix(T).init(allocator, hyper_param.attn_dim, hyper_param.seq_dim);
             retval.query_vect = try allocator.alloc(T, hyper_param.attn_dim);
             retval.query = try Matrix(T).init(allocator, hyper_param.seq_len, hyper_param.attn_dim);
             
@@ -167,22 +201,38 @@ pub fn Attention(comptime T: type) type {
 
             retval.value_matrix = try Matrix(T).init(allocator, hyper_param.out_dim, hyper_param.ctx_dim);
             retval.value_vect = try allocator.alloc(T, hyper_param.out_dim);
-            retval.value = try Matrix(T).init(allocator, hyper_param.ctx_len, hyper_param.out_dim);
+            retval.value = try Matrix(T).init(allocator, hyper_param.out_dim, hyper_param.ctx_len);
+
+            retval.out = try Matrix(T).init(allocator, hyper_param.seq_len, hyper_param.out_dim);
 
             return retval;
         }
 
-        pub fn calulate(self: Self, seq: Matrix(T), context: Matrix(T)) Matrix(T) {
+        pub fn calulate(self: Self, seq: Matrix(T), context: Matrix(T)) !Matrix(T) {
             
             // This can be parallelized
             try affine(T, .{.mat = self.query_matrix, .input = seq,
                 .vect = self.query_vect, .output = self.query});
+            std.debug.print("got query\n", .{});
+
             try affine(T, .{.mat = self.key_matrix, .input = context,
                 .vect = self.key_vect, .output = self.key});
-            try affine(T, .{.mat = self.value_matrix, .input = context,
-                .vect = self.key_vect, .output = self.key});
+            std.debug.print("got key\n", .{});
 
-            return out_value;
+            try affine2(T, .{.mat = context, .input = self.value_matrix,
+                .vect = self.value_vect, .output = self.value});
+            std.debug.print("got value\n", .{});
+
+            try matprod(T, self.key, self.query, self.score);
+            std.debug.print("got score\n", .{});
+            
+            softmax(T, self.score);
+            std.debug.print("softmax of score\n", .{});
+
+            try matprod(T, self.value, self.score, self.out);
+            std.debug.print("got out\n", .{});
+
+            return self.out;
         }
     };
 }
@@ -275,34 +325,92 @@ pub fn EDTransformer(comptime T: type) type {
 test "matmult" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+
+    var seqv = [_]f16{
+        1, 2, 3,
+        2, 1, 3,
+        4, 3, 1,
+        4, 4, 1,
+        5, 3, 5,
+        2, 4, 3,
+        1, 1, 1,
+    };
+    const seq = Matrix(f16){
+        .height = 7,
+        .width = 3,
+        .ptr = &seqv,
+    };
+
+    var ctxv = [_]f16{
+        1, 2,
+        3, 4,
+        5, 6,
+        7, 8,
+        9, 1,
+        2, 3,
+        4, 5,
+        6, 7,
+    };
+    const ctx = Matrix(f16){
+        .height = 8,
+        .width = 3,
+        .ptr = &ctxv,
+    };
+
+
+    const att = try Attention(f16).init(allocator, .{
+        .seq_dim = 3, .ctx_dim = 2, .attn_dim = 4, .out_dim = 5, .seq_len = 7, .ctx_len = 8});
     
-    var m1 = try Matrix(i32).init(allocator, 2, 3);
-    var m2 = try Matrix(i32).init(allocator, 4, 3);
-    const res = try Matrix(i32).init(allocator, 4, 2);
+    att.query_matrix.ptr[0] = 0;
+    att.query_matrix.ptr[1] = 1;
+    att.query_matrix.ptr[2] = 2;
+    att.query_matrix.ptr[3] = 3;
+    att.query_matrix.ptr[4] = 4;
+    att.query_matrix.ptr[5] = 5;
+    att.query_matrix.ptr[6] = 6;
+    att.query_matrix.ptr[7] = 7;
+    att.query_matrix.ptr[8] = 8;
+    att.query_matrix.ptr[9] = 9;
+    att.query_matrix.ptr[10] = 10;
+    att.query_matrix.ptr[11] = 11;
 
-    var vect = [2]i32{1, 1};
+    att.query_vect[0] = 3;
+    att.query_vect[1] = 1;
+    att.query_vect[2] = 2;
+    att.query_vect[3] = 4;
 
-    m1.ptr[0] = 2;
-    m1.ptr[1] = 3;
-    m1.ptr[2] = 4;
-    m1.ptr[3] = 1;
-    m1.ptr[4] = 2;
-    m1.ptr[5] = 1;
+    att.key_matrix.ptr[0] = 1; 
+    att.key_matrix.ptr[1] = 2; 
+    att.key_matrix.ptr[2] = 3; 
+    att.key_matrix.ptr[3] = 4; 
+    att.key_matrix.ptr[4] = 5; 
+    att.key_matrix.ptr[5] = 6; 
+    att.key_matrix.ptr[6] = 7; 
+    att.key_matrix.ptr[7] = 8; 
 
-    m2.ptr[0] = 3;
-    m2.ptr[1] = 4;
-    m2.ptr[2] = 5;
-    m2.ptr[3] = 0;
-    m2.ptr[4] = 6;
-    m2.ptr[5] = 6;
-    m2.ptr[6] = 1;
-    m2.ptr[7] = -1;
-    m2.ptr[8] = 1;
-    m2.ptr[9] = 2;
-    m2.ptr[10] = 1;
-    m2.ptr[11] = 3;
+    att.key_vect.ptr[0] = 4;
+    att.key_vect.ptr[1] = 9;
+    att.key_vect.ptr[2] = 6;
+    att.key_vect.ptr[3] = 1;
 
-    try affine(i32, .{.mat = m1, .input = m2, .vect = (&vect)[0..], .output = res});
-    matprint(i32, res);
+    att.value_matrix.ptr[0] = -9;
+    att.value_matrix.ptr[1] = -8;
+    att.value_matrix.ptr[2] = -7;
+    att.value_matrix.ptr[3] = -6;
+    att.value_matrix.ptr[4] = -5;
+    att.value_matrix.ptr[5] = -4;
+    att.value_matrix.ptr[6] = -3;
+    att.value_matrix.ptr[7] = -2;
+    att.value_matrix.ptr[8] = -1;
+    att.value_matrix.ptr[9] = 0;
+
+    att.value_vect[0] = 3;
+    att.value_vect[1] = -1;
+    att.value_vect[2] = 10;
+    att.value_vect[3] = 11;
+    att.value_vect[4] = 3;
+
+    _ = try att.calulate(seq, ctx);
+    matprint(f16, att.out);
 
 }
